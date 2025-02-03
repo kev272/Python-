05ddfc7f93cc82201df3d6ad1efeffefc5a2431b
Kev272 <k95398049@gmail.com>
2025-02-03 13:46:36 +0300

    third commit

---
+++ Introduction.py
@@ -0,0 +1,6 @@
+#print is a method used display a value
+
+print("Kevin")
+print("emobilis")
+print(39)
+print(100*2)
---
+++ Variable.py
@@ -0,0 +1,19 @@
+#Variable is a container used to store a value
+
+number = 10
+firstname = "Kevin"
+lastname = "Koech"
+language = "Python"
+a = 10
+b = 15
+c = 25
+print(number)
+print(firstname)
+print(lastname)
+
+print(firstname , lastname)
+print(language, "is a web programming language")
+print(a+b)
+#the sum of 10 and 15 1s 25
+
+print("the sum of" , a , "and" , b , "is" , c)
